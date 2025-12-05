d20c9994bb7b501f2641ff2c12457a18bae2bd2d
goblgobl01 <133246212+goblgobl01@users.noreply.github.com>
2025-12-05 14:25:31 +0000

    lao

--- cpp01/ex01/ZombieHorde.cpp
+++ cpp01/ex01/ZombieHorde.cpp
@@ -3,13 +3,13 @@
 Zombie* zombieHorde( int N, std::string name )
 {
 	if (N == 0)
-		return (NULL);
+		return NULL;
 	Zombie *ptr;
 
 	ptr = new(std::nothrow) Zombie[N];
 	if (!ptr)
-		return (NULL);
+		return NULL;
 	for (int i; i < N; i++)
 		ptr[i].set_name(name);
-	return (ptr);
+	return ptr;
 }
--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -3,8 +3,13 @@
 int main()
 {
 	int N = 10;
-	Zombie *zombies = zombieHorde(10, "BOBS");
-	for (int i = 0; i < N; i++)
-		zombies[i].announce(); 
+	Zombie *zombies = zombieHorde(N, "BOBS");
+	if (zombies)
+	{
+		for (int i = 0; i < N; i++)
+			zombies[i].announce(); 
+	}
+	else
+		std::cout<<"bad allocation maybe"<<std::endl;
 	delete[] zombies;
 }
