687e3bf8b1ed17b2da923b039458c6863084890f
goblgobl01 <133246212+goblgobl01@users.noreply.github.com>
2026-02-12 21:07:24 +0000

    alo

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,52 +1,54 @@
 #include "Bureaucrat.hpp"
 
-// Bureaucrat::Bureaucrat(): name("default"), grade(150) {}
+Bureaucrat::Bureaucrat(): name("default"), grade(150) {}
 
-// Bureaucrat::Bureaucrat(std::string _name, int _grade) : name(_name)
-// {
-// 	if (_grade < 0)
-// 		throw 
-// }
+Bureaucrat::Bureaucrat(std::string _name, int _grade) : name(_name)
+{
+	if (_grade < 0)
+		throw Bureaucrat::GradeTooHighException();
+	if (_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->grade = _grade;
+}
 
-// Bureaucrat::Bureaucrat(Bureaucrat &cp)
-// {
+Bureaucrat::Bureaucrat(Bureaucrat &cp): name(cp.name), grade(cp.grade) {}
 
-// }
+Bureaucrat &Bureaucrat::operator=(Bureaucrat &obj)
+{
+	this->grade = obj.grade;
+	return (*this);
+}
 
-// Bureaucrat &Bureaucrat::operator=(Bureaucrat &obj)
-// {
+Bureaucrat::~Bureaucrat() {}
 
-// }
+const std::string Bureaucrat::getName()
+{
+	return (this->name);
+}
 
-// Bureaucrat::~Bureaucrat()
-// {
+int Bureaucrat::getGrade()
+{
+	return (this->grade);
+}
 
-// }
+void Bureaucrat::increment()
+{
+	if ((this->grade - 1) < 0)
+		throw Bureaucrat::GradeTooHighException();
+	this->grade++;
+}
 
-// std::string Bureaucrat::getName()
-// {
-
-// }
-
-// int Bureaucrat::getGrade()
-// {
-
-// }
-
-// void Bureaucrat::increment()
-// {
-
-// }
-
-// void Bureaucrat::decrement()
-// {
-
-// }
+void Bureaucrat::decrement()
+{
+	if ((this->grade + 1) > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->grade--;
+}
 
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-    return "Bureaucrat grade is too high (below 1)!";
+	return "Bureaucrat grade is too high (below 1)!";
 }
 
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-    return "Bureaucrat grade is too low (above 150)!";
+	return "Bureaucrat grade is too low (above 150)!";
 }
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -5,24 +5,24 @@
 #include <exception>
 class Bureaucrat {
 	private :
-		// const std::string	name;
-		// int					grade;
+		const std::string	name;
+		int					grade;
 	public :
-		// Bureaucrat();
-		// Bureaucrat(std::string _name, int _grade);
-		// Bureaucrat(Bureaucrat &cp);
-		// Bureaucrat &operator=(Bureaucrat &obj);
-		// ~Bureaucrat();
-		// std::string getName();
-		// int getGrade();
-		// void increment();
-		// void decrement();
+		Bureaucrat();
+		Bureaucrat(std::string _name, int _grade);
+		Bureaucrat(Bureaucrat &cp);
+		Bureaucrat &operator=(Bureaucrat &obj);
+		~Bureaucrat();
+		const std::string getName();
+		int getGrade();
+		void increment();
+		void decrement();
 		class GradeTooHighException : public std::exception {
 			public:
-				virtual const char* what() const throw();
+				const char* what() const throw();
 		};
 		class GradeTooLowException : public std::exception {
 			public:
-				virtual const char* what() const throw();
+				const char* what() const throw(); // throw at the end promise that this function will not throw any exceptions
 		};
 };
