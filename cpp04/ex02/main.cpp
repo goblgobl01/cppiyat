e2239925d19a956292df09ca1e917d04eaca205e
goblgobl01 <maarafimohammed@gmail.com>
2026-02-14 18:04:29 +0100

    alo

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-	const Animal* meta = new Animal();
+	// const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	std::cout << j->getType() << " " << std::endl;
